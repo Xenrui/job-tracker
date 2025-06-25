d76b1b0bb88149336d0223243ed4d17fe64b994d
Xenon <joshua123.jdr@gmail.com>
2025-06-25 19:58:52 +0800

    initial commit

---
+++ src/components/LoginForm.jsx
@@ -0,0 +1,17 @@
+
+import React from 'react'
+
+const LoginForm = () => {
+  return (
+    <div>
+      <div>
+        <h2>asdf</h2>
+      </div>
+      <div>
+
+      </div>
+    </div>
+  )
+}
+
+export default LoginForm  
